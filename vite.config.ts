b625a41374d351afb7dee7033f95de82d01384cf
Andrew <and.demydenko@gmail.com>
2024-11-07 09:52:30 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base:
     process.env.NODE_ENV === 'production' ? '/english-dictionary-app/' : '/',
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html',
+          dest: './',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
